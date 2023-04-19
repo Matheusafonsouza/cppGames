03b7ae8ce01eb89a8c8930056e4ad53df85fd564
Matheus Afonso <matheusafonsouza@gmail.com>
2023-04-19 00:04:57 -0300

    feat: add secret number

--- game.cpp
+++ game.cpp
@@ -6,4 +6,7 @@ int main () {
     cout << "***********************" << endl;
     cout << "* Welcome to the game *" << endl;
     cout << "***********************" << endl;
+
+    int secretNumber = 32;
+    cout << "The secret number is " << secretNumber << endl;
 }
