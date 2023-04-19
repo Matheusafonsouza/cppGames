3280384d107bbc8bfd8e963f159e34b0ba79bc83
Matheus Afonso <matheusafonsouza@gmail.com>
2023-04-19 00:09:54 -0300

    chore: use secret number as constant

--- game.cpp
+++ game.cpp
@@ -7,19 +7,19 @@ int main () {
     cout << "* Welcome to the game *" << endl;
     cout << "***********************" << endl;
 
-    int secretNumber = 32;
-    cout << "The secret number is " << secretNumber << endl;
+    const int SECRET_NUMBER = 32;
+    cout << "The secret number is " << SECRET_NUMBER << endl;
 
     int number;
     cout << "What is your number?" << endl;
     cin >> number;
     cout << "Your number is " << number << endl;
 
-    if (number == secretNumber) {
+    if (number == SECRET_NUMBER) {
         cout << "Congratulations! Your number is correct :)" << endl;
-    } else if (number > secretNumber) {
+    } else if (number > SECRET_NUMBER) {
         cout << "Wrong choice! Your number is bigger than the secret number." << endl;
-    } else if (number < secretNumber) {
+    } else if (number < SECRET_NUMBER) {
         cout << "Wrong choice! Your number is smaller than the secret number." << endl;
     }
 }
