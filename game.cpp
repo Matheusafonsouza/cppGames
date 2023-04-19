b8a0e54f22ccbcd48705fac880c9e7bdc0e788a1
Matheus Afonso <matheusafonsouza@gmail.com>
2023-04-19 00:23:12 -0300

    feat: add difficulty

--- game.cpp
+++ game.cpp
@@ -7,12 +7,29 @@ int main () {
     cout << "* Welcome to the game *" << endl;
     cout << "***********************" << endl;
 
+    cout << "Choose the difficulty" << endl;
+    cout << "Easy (E), Normal (N), Hard (H)" << endl;
+
+    char difficulty;
+    cin >> difficulty;
+
+    int playerAttemptsLimit;
+
+    if (difficulty == 'E') {
+        playerAttemptsLimit = 15;
+    } else if (difficulty == 'M') {
+        playerAttemptsLimit = 10;
+        
+    } else if (difficulty == 'H') {
+        playerAttemptsLimit = 5;
+    }
+
     const int SECRET_NUMBER = 32;
     cout << "The secret number is " << SECRET_NUMBER << endl;
 
     int number, attempts = 0;
     double points = 1000.0; 
-    while(true) {
+    while(attempts < playerAttemptsLimit) {
         attempts++;
         cout << "What is your number?" << endl;
         cin >> number;
@@ -30,8 +47,13 @@ int main () {
         double lossPoints = abs(number - SECRET_NUMBER) / 2.0;
         points = points - lossPoints;
     }
-    cout << "You answered the correct number in " << attempts << " attempts!" << endl;
-    cout.precision(2);
-    cout << fixed;
-    cout << "Your pontuation was " << points << endl;
+
+    if (attempts < playerAttemptsLimit) {
+        cout << "You answered the correct number in " << attempts << " attempts!" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Your pontuation was " << points << endl;
+    } else {
+        cout << "You losed :( try again!" << endl;
+    }
 }
