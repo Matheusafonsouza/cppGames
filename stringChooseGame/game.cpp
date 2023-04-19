30916d70c5621d79defc2db96f72b2d8f2578b1b
Matheus Afonso <matheusafonsouza@gmail.com>
2023-04-19 00:32:39 -0300

    feat: add attempts and cin for word

--- stringChooseGame/game.cpp
+++ stringChooseGame/game.cpp
@@ -4,6 +4,34 @@
 using namespace std;
 
 int main () {
+    cout << "***********************" << endl;
+    cout << "* Welcome to the game *" << endl;
+    cout << "***********************" << endl;
+
     const string SECRET_WORD = "APPLE";
     cout << "The secred word is " << SECRET_WORD << endl;
+
+    string word;
+    int attempts = 0, playerAttemptsLimit = 10;
+    double points = 1000.0; 
+    while(attempts < playerAttemptsLimit) {
+        attempts++;
+        cout << "What is your word?" << endl;
+        cin >> word;
+        cout << "Your word is " << word << endl;
+
+        if (word == SECRET_WORD) {
+            cout << "Congratulations! Your word is correct :)" << endl;
+            break;
+        }
+    }
+
+    if (attempts < playerAttemptsLimit) {
+        cout << "You answered the correct word in " << attempts << " attempts!" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Your pontuation was " << points << endl;
+    } else {
+        cout << "You losed :( try again!" << endl;
+    }
 }
