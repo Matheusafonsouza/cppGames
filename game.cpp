fb7c0e93408b25da83ea02ae3a1f581fa5252afd
Matheus Afonso <matheusafonsouza@gmail.com>
2023-04-19 00:08:37 -0300

    feat: add number question

--- game.cpp
+++ game.cpp
@@ -9,4 +9,17 @@ int main () {
 
     int secretNumber = 32;
     cout << "The secret number is " << secretNumber << endl;
+
+    int number;
+    cout << "What is your number?" << endl;
+    cin >> number;
+    cout << "Your number is " << number << endl;
+
+    if (number == secretNumber) {
+        cout << "Congratulations! Your number is correct :)" << endl;
+    } else if (number > secretNumber) {
+        cout << "Wrong choice! Your number is bigger than the secret number." << endl;
+    } else if (number < secretNumber) {
+        cout << "Wrong choice! Your number is smaller than the secret number." << endl;
+    }
 }
