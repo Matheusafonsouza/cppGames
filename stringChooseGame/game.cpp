b89cfef76ea7b4bbc3ecaac357d54c2a6900362d
Matheus Afonso <matheusafonsouza@gmail.com>
2023-04-19 00:37:22 -0300

    feat: function to check if letter is on secret word

--- stringChooseGame/game.cpp
+++ stringChooseGame/game.cpp
@@ -3,26 +3,38 @@
 
 using namespace std;
 
+const string SECRET_WORD = "APPLE";
+
+bool checkLetterExists(char playerChar) {
+    for(int i = 0; i < SECRET_WORD.size(); i++) {
+        if (playerChar == SECRET_WORD[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main () {
     cout << "***********************" << endl;
     cout << "* Welcome to the game *" << endl;
     cout << "***********************" << endl;
 
-    const string SECRET_WORD = "APPLE";
     cout << "The secred word is " << SECRET_WORD << endl;
 
-    string word;
+    char playerChar;
     int attempts = 0, playerAttemptsLimit = 10;
     double points = 1000.0; 
     while(attempts < playerAttemptsLimit) {
         attempts++;
-        cout << "What is your word?" << endl;
-        cin >> word;
-        cout << "Your word is " << word << endl;
+        cout << "What is your char?" << endl;
+        cin >> playerChar;
+        cout << "Your char is " << playerChar << endl;
 
-        if (word == SECRET_WORD) {
-            cout << "Congratulations! Your word is correct :)" << endl;
+        if (checkLetterExists(playerChar)) {
+            cout << "Congratulations! Your char is correct :)" << endl;
             break;
+        } else {
+            "Wrong choice! Try again.";
         }
     }
 
