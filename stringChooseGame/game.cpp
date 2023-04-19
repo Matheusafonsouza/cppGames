aa4f409286388ca595f73fdd7012e4c4a20bdfba
Matheus Afonso <matheusafonsouza@gmail.com>
2023-04-19 00:29:46 -0300

    feat: string choose game secret word

---
+++ stringChooseGame/game.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    const string SECRET_WORD = "APPLE";
+    cout << "The secred word is " << SECRET_WORD << endl;
+}
